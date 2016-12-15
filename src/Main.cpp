26bea60e16a2d3309391abe5765566f3ccd3627a
Elliot <Elliot@dhcp-32-181.eduroam.ec-m.fr>
2016-12-15 10:52:22 +0100

    # WARNING: head commit changed in the meantime
    Coucou

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Main.cpp
+// Author      : Elliot Drees
+// Version     :
+// Copyright   : Blahblah
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
