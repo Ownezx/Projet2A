a96aff83f63c00811bf194d3daf59eb050f4ad10
Elliot <Elliot@dhcp-32-181.eduroam.ec-m.fr>
2016-12-18 00:08:10 +0100

    Programmation des premières fonctions pour vector

--- src/Classes/Vector.cpp
+++ src/Classes/Vector.cpp
@@ -6,16 +6,78 @@
  */
 
 #include "Vector.h"
+#include <math.h>
 
 namespace std {
 
-Vector::Vector(int size) {
-	// TODO Auto-generated constructor stub
 
+
+Vector::Vector(int Size) {
+	size = Size;
+	values = new float[size];
+}
+
+Vector::Vector(int Size, float* Values) {
+	size = Size;
+	values = Values;
 }
 
 Vector::~Vector() {
 	// TODO Auto-generated destructor stub
 }
 
+
+void Vector::setValue(int pos, float value) {
+	if(pos<size){
+		values[pos]=value;
+	}
+	else{
+		cout<<"Error : cannot assign value to higher dimentions"<<endl;
+		throw -1;
+	}
+}
+
+int Vector::getSize() {
+	return size;
+}
+
+float Vector::scalar(const Vector& A) {
+	if(this->size==A.size){
+		float sum = 0;
+		for(int i=0; i< size; i++){
+			sum+=values[i]*A.values[i];
+		}
+		return sum;
+	}
+	else{
+		cout<<"Error : cannot scalar vector with different dimensions"<<endl;
+		throw -1;
+	}
+}
+
+float Vector::norm() {
+	float sum = 0;
+	for(int i=0; i< size; i++){
+				sum+=values[i]*values[i];
+			}
+	sum = sqrt(sum);
+	return sum;
+}
+
+int Vector::max() {
+	float max = values[0];
+	int posmax = 0;
+	for(int i=1; i< size; i++){
+				if (values[i]>max){
+					posmax=i;
+					max=values[i];
+				}
+			}
+	return posmax;
+}
+
+
 } /* namespace std */
+
+
+
